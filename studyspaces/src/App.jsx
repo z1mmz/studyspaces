be60f95bdc6bff955ae9f2861bcd08d84002df61
Wolf Zimmermann <z1mmz@users.noreply.github.com>
2025-02-09 23:17:18 +0000

    leaflet setup

--- studyspaces/src/App.jsx
+++ studyspaces/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import Map from './components/Map'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +10,7 @@ function App() {
 
     <div>
       <h1>Welcome</h1>
+      <Map/>
     </div>
 
   )
