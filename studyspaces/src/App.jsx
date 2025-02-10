662ccfc1803a3e2d24b0bae7e247561298553ce9
Wolf Zimmermann <wolf.zimmermann@educaiton.vic.gov.au>
2025-02-10 15:15:51 +1100

    Moved location fetch to app

--- studyspaces/src/App.jsx
+++ studyspaces/src/App.jsx
@@ -1,16 +1,34 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 
 import './App.css'
 import Map from './components/Map'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
+  const [position, setPosition] = useState({lat:-37.8136,lon: 144.9631})
+  const [spaces,setSpaces] = useState([])
+   
 
+
+  useEffect(() =>{
+      fetchUserLocation()
+  },[])
+
+  const fetchUserLocation = () =>{
+
+        console.log("getting pos")
+         navigator.geolocation.getCurrentPosition((p)=>{
+          
+          setPosition({lat:p.coords.latitude,lon:p.coords.longitude})
+          console.log(p)
+         })
+      
+  }
   return (
 
     <div>
       <h1>Welcome</h1>
-      <Map/>
+      <Map pos={position} spaces={spaces}/>
     </div>
 
   )
--- studyspaces/src/components/Map.jsx
+++ studyspaces/src/components/Map.jsx
@@ -1,39 +1,28 @@
 import { MapContainer, TileLayer, useMap,Marker,Popup} from 'react-leaflet'
 import { useEffect,useState} from 'react'
 import 'leaflet/dist/leaflet.css'
-function UserLocation() {
-    const [position, setPosition] = useState(null)
+ const FocusLocation = ({pos}) => {
     const map = useMap()
-    useEffect(() =>{
-        fetchUserLocation()
-    },[])
 
-    const fetchUserLocation = () =>{
-        if(!position){
-            map.locate().on('locationfound', 
-            (e) =>{
-                console.log(e)
-                setPosition(e.latlng)
-                map.flyTo(e.latlng, map.getZoom())
-            })
-        }
+    if(pos){
+        console.log(pos)
+        map.flyTo([pos.lat,pos.lon],map.getZoom())
     }
+
 
   return null
 
   }
-const Map = () =>{
-
-    // map.locate()
-   
+const Map = ({pos,spaces}) =>{
+    
     return(
 
-                <MapContainer style={{ width: '100%', height: '600px' }} center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+                <MapContainer style={{ width: '100%', height: '600px' }} center={[pos.lat,pos.lon]} zoom={13} scrollWheelZoom={false}>
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                <UserLocation />
+                <FocusLocation pos={pos} />
                 <Marker position={[51.505, -0.09]}>
                     <Popup>
                     A pretty CSS3 popup. <br /> Easily customizable.
